16f862e580af7c1c2b97b2b34b35e3469c705c98
Rishi <rishi@Major-Toms-Macbook-Pro.local>
2022-07-18 00:45:40 +0400

    initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pkpass/pkpass' => array(
+            'pretty_version' => 'v1.2.7',
+            'version' => '1.2.7.0',
+            'reference' => '282b4076e3e046a94538e50e755d580f1b817a56',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../pkpass/pkpass',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
