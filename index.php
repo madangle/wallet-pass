64dfa19b2bfb126b6c1cbb09b13bdd7622691280
Rishikesh Krishnan <ec.rishikeshnair@gas.gov.ae>
2022-07-19 11:42:56 +0400

    added assets

--- index.php
+++ index.php
@@ -9,62 +9,71 @@ $pass = new PKPass('./DATA/Certificatesboth.p12', '123');
 
 // Pass content
 $data = [
-    'description' => 'RISHIKESH KRISHNAN TEST PASS',
-    'formatVersion' => 1,
-    'organizationName' => 'Flight Express V2',
-    'passTypeIdentifier' => 'pass.com.sp.evententry', // Change this!
-    'serialNumber' => '12345678',
-    'teamIdentifier' => 'MS7X7622VQ', // Change this! 
-    'boardingPass' => [
-        'primaryFields' => [
-            [
-                'key' => 'origin',
-                'label' => 'San Francisco',
-                'value' => 'SFO',
-            ],
-            [
-                'key' => 'destination',
-                'label' => 'London',
-                'value' => 'LHR',
-            ],
-        ],
-        'secondaryFields' => [
-            [
-                'key' => 'gate',
-                'label' => 'Gate',
-                'value' => 'F12',
-            ],
-            [
-                'key' => 'date',
-                'label' => 'Departure date',
-                'value' => '07/11/2012 10:22',
-            ],
-        ],
-        'backFields' => [
-            [
-                'key' => 'passenger-name',
-                'label' => 'Passenger',
-                'value' => 'John Appleseed',
-            ],
-        ],
-        'transitType' => 'PKTransitTypeAir',
+  'formatVersion' => 1,
+  'passTypeIdentifier' => 'pass.com.sp.evententry', 
+  'serialNumber' => '12345678',
+  'webServiceURL' => 'https://example.com/passes/',
+  'authenticationToken' => 'vxwxd7J8AlNNFPS8k0a0FfUFtq0ewzFdc',
+  'teamIdentifier' => 'MS7X7622VQ',
+  'organizationName' => 'Chaîne des Rôtisseurs',
+  'description' => 'Association Modiale de la Gastronomie',
+  'foregroundColor' => 'rgb(33, 37, 41)',
+  'backgroundColor' => 'rgb(168, 147, 92)',
+  // 'logoText' => 'Association Modiale de la Gastronomie',
+  'logoText' => 'Chaîne des Rôtisseurs',
+  'generic' => [
+    'primaryFields' => [
+      [
+        'key' => 'member',
+        'value' => 'Mr. Dailos RODRIGUEZ'
+      ],
     ],
-    'barcode' => [
-        'format' => 'PKBarcodeFormatQR',
-        'message' => 'Flight-GateF12-ID6643679AH7B',
-        'messageEncoding' => 'iso-8859-1',
+    'secondaryFields' => [
+      [
+        'key' => 'subtitle',
+        'label' => 'Baillliage des Emirats Arabes Unis',
+        'value' => '971 250 G1  103503',
+      ]
     ],
-    'backgroundColor' => 'rgb(32,110,247)',
-    'logoText' => 'Flight info',
-    'relevantDate' => date('Y-m-d\TH:i:sP')
+    'auxiliaryFields' => [
+      [
+        'key' => 'subtitle1',
+        'label' => 'MEMBERSHIP TYPE',
+        'value' => 'PREMIUM',
+      ],
+      [
+        'key' => 'subtitle2',
+        'label' => 'VALID THRU',
+        'value' => 'DEC 2022',
+        'textAlignment' => 'PKTextAlignmentRight'
+      ]
+    ]
+  ],
+  'locations' => [
+    [
+      'longitude' => -122.3748889,
+      'latitude' => 37.6189722
+    ],
+    [
+      'longitude' => -122.03118,
+      'latitude' => 37.33182
+    ]
+  ],
+  'barcode' => [
+    'format' => 'PKBarcodeFormatQR',
+    'message' => 'https://www.chainedesrotisseurs.com/',
+    'messageEncoding' => 'iso-8859-1',
+  ]
 ];
+
+
 $pass->setData($data);
 
 // Add files to the pass package
-$pass->addFile('./examples/images/icon.png');
-$pass->addFile('./examples/images/icon@2x.png');
-$pass->addFile('./examples/images/logo.png');
-
+$pass->addFile('./assets/icon.png');
+$pass->addFile('./assets/icon@2x.png');
+$pass->addFile('./assets/logo.png');
+// $pass->addFile('./assets/thumbnail.png');
 // Create and output the pass
 if(!$pass->create(true)) {
     echo 'Error: ' . $pass->getError();
